62c447b09c63df3e93da219c7f39a7227e73ef88
Miguel Pereira <110423169+MiguelPereiraDantas@users.noreply.github.com>
2023-10-16 01:18:57 -0300

    Update and rename models.py to serializers.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Game(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    release_date = models.DateField()
